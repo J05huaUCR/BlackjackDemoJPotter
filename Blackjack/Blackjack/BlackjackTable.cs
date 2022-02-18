98c19f1c33f715f437223121a88550f6cee7cf7e
J05huaUCR <J05huaUCR@users.noreply.github.com>
2022-02-18 11:24:16 -0800

    Build 1.0

---
+++ Blackjack/Blackjack/BlackjackTable.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blackjack
+{
+    
+    public partial class BlackjackTable : Form
+    {
+        private Game blackJack;
+        public Dictionary<string, Panel> DeckUI = new Dictionary<string, Panel>();
+        int cardWidth = 144;
+        Point dealerOrigin = new Point(80, 100);
+        Point playerOrigin = new Point(80, 436);
+        Config cfg = new Config();
+
+        public BlackjackTable()
+        {
+            InitializeComponent();
+            
+            blackJack = new Game();
+            gameStatusBox.Visible = false;
+            gameStatusBox.Text = blackJack.getInfo();
+            newUser.Visible = false;
+            renderDeck();
+            showDeck(false);
+            updateUI();
+        }
+
+        // Renders card deck to UI
+        public void renderDeck()
+        {
+
+            string cardID = "";
+            Point p = new Point(0,0);
+
+            // Traverse CardIDs enum to get image coordinates
+            foreach (var card in Enum.GetNames(typeof(CardIDs)))
+            {
+                // Get enum ID
+                cardID = card.ToString();
+
+                // Which suit
+                string whichRow = cardID.Substring(0, 1);
+                switch (whichRow)
+                {
+                    case "H":
+                        p.Y = (int)ImageY.H;
+                        break;
+
+                    case "D":
+                        p.Y = (int)ImageY.D;
+                        break;
+
+                    case "C":
+                        p.Y = (int)ImageY.C;
+                        break;
+
+                    case "S":
+                        p.Y = (int)ImageY.S;
+                        break;
+
+                    default:
+                        // Nothing
+                        break;
+
+                }
+
+                // Which Card
+                string whichCol = cardID.Substring(1, 1);
+                switch (whichCol)
+                { 
+                    case "2":
+                        p.X = (int)ImageX.C2;
+                        break;
+                    case "3":
+                        p.X = (int)ImageX.C3;
+                        break;
+                    case "41":
+                        p.X = (int)ImageX.C4;
+                        break;
+                    case "5":
+                        p.X = (int)ImageX.C5;
+                        break;
+                    case "6":
+                        p.X = (int)ImageX.C6;
+                        break;
+                    case "7":
+                        p.X = (int)ImageX.C7;
+                        break;
+                    case "8":
+                        p.X = (int)ImageX.C8;
+                        break;
+                    case "9":
+                        p.X = (int)ImageX.C9;
+                        break;
+                    case "0":
+                        p.X = (int)ImageX.C0;
+                        break;
+                    case "J":
+                        p.X = (int)ImageX.CJ;
+                        break;
+                    case "Q":
+                        p.X = (int)ImageX.CQ;
+                        break;
+                    case "K":
+                        p.X = (int)ImageX.CK;
+                        break;
+                    case "A":
+                        p.X = (int)ImageX.CA;
+                        break;
+
+                    default:
+                        // Nothing
+                        break;
+
+                }
+
+                Panel pnl = new Panel();
+                pnl.Visible = true;
+                pnl.Size = new Size(143, 209);
+                pnl.Location = p;
+                pnl.BorderStyle = BorderStyle.None;
+                pnl.BackColor = Color.Transparent;
+
+                PictureBox pic = new PictureBox();
+                pic.Location = p;
+                pic.Size = new Size(1866, 840);
+                pic.BackgroundImage = Properties.Resources.Deck_s;
+                pic.BackgroundImageLayout = ImageLayout.None;
+                pic.SizeMode = PictureBoxSizeMode.Normal;
+                pnl.Controls.Add(pic);
+                this.Controls.Add(pnl);
+
+                // Add to Deck
+                DeckUI.Add(cardID, pnl);
+            }
+
+            resetDeck();            
+        }
+
+        private void resetDeck()
+        {
+            Panel p;
+            Point pt = new Point(0, 300);
+            int i = 1014;
+            foreach (var item in DeckUI)
+            {
+                pt.X = i;
+                
+                p = item.Value;
+                p.Location = pt;
+                i -= 20;
+            }
+        }
+
+        private void showDeck(bool b)
+        {
+            Panel p;
+            foreach (var item in DeckUI)
+            {
+                p = item.Value;
+                p.Visible = b;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void BlackjackTable_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_MouseUp(object sender, MouseEventArgs e)
+        {
+            showAboutBox();
+        }
+        
+
+        public void showAboutBox()
+        {
+            MessageBox.Show(cfg.Attribution);
+        }
+
+        private void playerNameTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enableGameMenu(bool b)
+        {
+            gameMenuDeal.Enabled = b;
+            gameMenuStand.Enabled = b;
+            btnHit.Enabled = b;
+            btnStand.Enabled = b;
+        }
+
+        private void newUserButton_Click(object sender, EventArgs e)
+        {
+            if (playerNameTB.Text != "")
+            {
+                blackJack.addPlayer(playerNameTB.Text);
+                newUser.Visible = false;
+                gameMenuAddUser.Enabled = false;
+                gameMenuItemNew.Enabled = true;
+                updateUI();
+            } else
+            {
+                playerNameTB.Text = cfg.ErrorBadName;
+            }
+        }
+
+        private void gameStatusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Toggle Visibility
+            gameStatusToolStripMenuItem.Checked = !gameStatusBox.Visible;
+            gameStatusBox.Visible = !gameStatusBox.Visible;
+        }
+
+        private void gameMenuExit_Click(object sender, EventArgs e)
+        {
+            if (blackJack.getState() == GameStates.PLAYER_TURN || blackJack.getState() == GameStates.DEALER_TURN)
+            {
+                // Game not finished
+                MessageBox.Show("Game not over!");
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void gameMenuAddUser_Click(object sender, EventArgs e)
+        {
+            if (blackJack.getPlayerCount() < 2)
+            {
+                newUser.Visible = true;
+            }
+        }
+
+        private void gameMenuNew_Click(object sender, EventArgs e)
+        {
+            enableGameMenu(true);
+            blackJack.start();
+            updateUI();
+        }
+
+        private void gameMenuDeal_Click(object sender, EventArgs e)
+        {
+            blackJack.deal(1);
+            updateUI();
+        }
+
+        private void gameMenuStand_Click(object sender, EventArgs e)
+        {
+            blackJack.stand();
+            updateUI();
+            
+        }
+
+        private void setStatusBar()
+        {
+
+            string status = "";
+            int cardsLeft = 0;
+            string dealerName = cfg.DealerName;
+            int dealerScore = 0;
+            string playerName = cfg.PlayerName;
+            int playerScore = 0;
+
+
+            // Assignments
+            cardsLeft = blackJack.getCardsRemaining();
+            if (blackJack.getPlayerCount() > 0)
+            {
+                dealerName = blackJack.getPlayerName(0);
+                dealerScore = blackJack.getPlayerScore(0);
+            }
+
+            if (blackJack.getPlayerCount() > 1)
+            {
+                playerName = blackJack.getPlayerName(1);
+                playerScore = blackJack.getPlayerScore(1);
+            }
+
+            statusCardsRemaining.Text = "Cards remaining: " + cardsLeft.ToString();
+            status += "Scores: ";
+            status += dealerName + " has " + blackJack.getPlayerScore(0).ToString() + ", ";
+            status += playerName + " has " + playerScore.ToString();
+            statusScores.Text = status;
+        }
+
+        private void renderHands()
+        {
+
+            List<CardIDs> dealerHand;
+            List<CardIDs> playerHand;
+            Panel pnl;
+            string whichCard = "";
+
+            // Process Dealer Hand
+            dealerHand = blackJack.getHand(0);
+            for (int i = 0; i < dealerHand.Count(); i++)
+            {
+                whichCard = dealerHand[i].ToString();
+                pnl = DeckUI[whichCard];
+                pnl.Visible = true;
+                pnl.Location = new Point(dealerOrigin.X + (cardWidth * i), dealerOrigin.Y);
+            }
+
+            // Process Player hands
+            playerHand = blackJack.getHand(1);
+            for (int i = 0; i < playerHand.Count(); i++)
+            {
+                whichCard = playerHand[i].ToString();
+                pnl = DeckUI[whichCard];
+                pnl.Visible = true;
+                pnl.Location = new Point(playerOrigin.X + (cardWidth * i), playerOrigin.Y);
+            }
+        }
+
+        // Render UI based on game state
+        public void updateUI()
+        {
+
+            switch ( blackJack.getState() )
+            {
+
+                case GameStates.INIT:
+                    showDeck(false);
+                    break;
+
+                case GameStates.ADD_PLAYER:
+                    showDeck(false);
+                    break;
+
+                case GameStates.START:
+                    lblDealer.Text = blackJack.getPlayerName(0);
+                    lblPlayer.Text = blackJack.getPlayerName(1);
+                    
+                    gameStatusBox.Text = blackJack.getInfo();
+                    showDeck(false);
+                    renderHands();
+                    break;
+
+                case GameStates.DEAL:
+                    
+                    showDeck(false);
+                    renderHands();
+                    break;
+
+                case GameStates.PLAYER_TURN:
+                    lblDealer.Text = blackJack.getPlayerName(0);
+                    lblPlayer.Text = blackJack.getPlayerName(1);
+                    showDeck(false);
+                    renderHands();
+                    break;
+
+                case GameStates.DEALER_TURN:
+                    enableGameMenu(false);
+                    renderHands();
+                    break;
+
+                case GameStates.PLAYER_WIN:
+                    lblPlayer.Text = blackJack.getPlayerName(1) + cfg.MsgWin;
+                    enableGameMenu(false);
+                    renderHands();
+                    break;
+
+                case GameStates.DEALER_WIN:
+                    lblDealer.Text = blackJack.getPlayerName(0) + cfg.MsgWin;
+                    enableGameMenu(false);
+                    renderHands();
+                    break;
+
+                case GameStates.TIE:
+                    lblPlayer.Text = blackJack.getPlayerName(1) + cfg.MsgTie;
+                    renderHands();
+                    break;
+
+                default:
+                    // Do nothing
+                    break;
+                    
+            }
+            setStatusBar();
+            gameStatusBox.Text = blackJack.getInfo();
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            blackJack.deal(1);
+            updateUI();
+        }
+
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            blackJack.stand();
+            enableGameMenu(false);
+            updateUI();
+        }
+    }
+    
+    
+}
---
+++ Blackjack/Blackjack/Game.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+
+    enum GameStates
+    {
+        INIT,
+        ADD_PLAYER,
+        START,
+        DEAL,
+        PLAYER_TURN,
+        DEALER_TURN,
+        TIE,
+        PLAYER_WIN,
+        DEALER_WIN
+    }
+
+    class Game
+    {
+        private GameStates State = GameStates.INIT;
+        private List<Player> Players = new List<Player>();
+        private List<Deck> Shoe = new List<Deck>();
+        private int MaxPlayers = 2;
+        private int PlayerCount = 0;
+        private int DeckCount = 0;
+
+        public Game(int deckCount = 1, int maxPlayers = 2) {
+            setDeckCount(deckCount);
+            init();
+        }
+
+        private void init()
+        {
+            setState(GameStates.INIT);
+
+            // Populate Shoe and shuffle
+            for (int i = 0; i < getDeckCount(); i++)
+            {
+                // Create new Deck
+                Deck d = new Deck(Shoe.Count());
+
+                // Add to Shoe
+                Shoe.Add(d);
+            }
+
+            // Set up dealer
+            addPlayer("Dealer", true);
+
+            // Trigger add player
+            setState(GameStates.ADD_PLAYER);
+
+        }
+
+        public void addPlayer(String name, bool isDealer = false)
+        {
+            Player p = new Player(name, isDealer, Players.Count());
+            add(p);
+            if ( getPlayerCount() == getMaxPlayers() ) setState(GameStates.START);
+        }
+
+        private void add(Player p)
+        {
+            Players.Add(p);
+            setPlayerCount(getPlayerCount() + 1);
+            return;
+        }
+
+        public String getInfo()
+        {
+            String info = "";
+            info += "State: ";
+            info += State.ToString();
+            info += "\n";
+            info += "Players: \n";
+            for (int i = 0; i < Players.Count(); i++)
+            {
+                Player p = Players[i];
+                info += "\tPlayer " + (i + 1).ToString() + ": " + p.getName() + ", Score: " + p.getScore() + "\n";
+                info += "\tHand: " + p.getHandAsString() + "\n";
+            }
+            info += "Cards emaining: " + getCardsRemaining().ToString() + "\n";
+            return info;
+        }
+
+        private void setState(GameStates s)
+        {
+            State = s;
+        }
+
+        public GameStates getState()
+        {
+            return State;
+        }
+
+        public void shuffleCards()
+        {
+            foreach (Deck d in Shoe)
+            {
+                d.shuffle();
+            }
+        }
+
+        public int getCardsRemaining()
+        {
+            int cardCount = 0;
+            foreach (Deck d in Shoe)
+            {
+                cardCount += d.getCardsRemaining();
+            }
+            return cardCount;
+        }
+
+        private void setDeckCount(int d = 1)
+        {
+            if (d < 1) d = 1;
+            DeckCount = d;
+        }
+
+        private int getDeckCount()
+        {
+            return DeckCount;
+        }
+
+        private void setPlayerCount(int d = 1)
+        {
+            if (d < 1) d = 1;
+            PlayerCount = d;
+        }
+
+        public int getPlayerCount()
+        {
+            return PlayerCount;
+        }
+
+        private void setMaxPlayers(int m)
+        {
+            if (m < 2) m = 2;
+            MaxPlayers = m;
+        }
+
+        private int getMaxPlayers()
+        {
+            return MaxPlayers;
+        }
+
+        public void start()
+        {
+            setState(GameStates.START);
+
+            // to hold and process any cards from previous players
+            List<Card> playerHands;
+            Deck temp = Shoe.ElementAt(0);
+
+            // Return any cards in players hands
+            foreach(Player p in Players)
+            {
+                playerHands = p.returnCards();
+                while(playerHands.Count() > 0)
+                {
+                    temp.replace(playerHands.ElementAt(0));
+                    playerHands.RemoveAt(0);
+                }
+            }
+
+            // Shuffle each deck in the Shoe
+            shuffleCards();
+
+            // Deal 2 cards to each player
+            foreach (Player p in Players)
+            {
+                p.takeCard(Shoe.ElementAt(0).deal());
+                p.takeCard(Shoe.ElementAt(0).deal());
+            }
+
+            // Change state
+            setState(GameStates.PLAYER_TURN);
+
+        }
+
+        // Deal card to player
+        public void deal(int playerID)
+        {
+            if (playerID < getPlayerCount())
+            {
+                Player p = Players.ElementAt(playerID);
+                p.takeCard(Shoe.ElementAt(0).deal());
+            }
+            gameResults();
+        }
+
+        public void deal()
+        {
+            // Determine game state
+            gameResults();
+        }
+
+        private void gameResults()
+        {
+            Player dealer;
+            Player player;
+            if (Players.Count() > 0) dealer = Players.ElementAt(0); else return;
+            if (Players.Count() > 1) player = Players.ElementAt(1); else return;
+
+            // Check whose turn it is
+            if ( getState() == GameStates.PLAYER_TURN )
+            {
+                // Player Turen
+                if (player.getScore() == 21 )
+                {
+                    setState(GameStates.PLAYER_WIN);
+                } else if (player.getScore() > 21)
+                {
+                    setState(GameStates.DEALER_WIN);
+                }
+
+            } else
+            {
+                // Dealer Turn
+                if (dealer.getScore() > 21)
+                {
+                    // Dealer bust
+                    setState(GameStates.PLAYER_WIN);
+                }
+                else if (player.getScore() == dealer.getScore())
+                {
+                    setState(GameStates.TIE);
+                }
+                else if (player.getScore() > dealer.getScore())
+                {
+                    setState(GameStates.PLAYER_WIN);
+                }
+                else if (player.getScore() < dealer.getScore() )
+                {
+                    setState(GameStates.DEALER_WIN);
+                }
+            }
+
+            
+        }
+
+        public void stand()
+        {
+
+            setState(GameStates.DEALER_TURN);
+            int score = Players.ElementAt(0).getScore(); ;
+            while (score < 17) {
+                deal(0);
+                score += Players.ElementAt(0).getScore();
+            }
+            gameResults();
+        }
+
+        public string getPlayerName(int i)
+        {
+            string name = "<player>";
+            if ( i < getPlayerCount() )
+            {
+                name = Players[i].getName();
+            }
+            return name;
+        }
+
+        public int getPlayerScore(int i)
+        {
+            int score = 0;
+            if (i < getPlayerCount())
+            {
+                score = Players[i].getScore();
+            }
+            return score;
+        }
+        
+        public List<CardIDs> getHand(int i)
+        {
+            if (i < getPlayerCount() )
+            {
+                return Players[i].getHand();
+            }
+            List<CardIDs> hand = new List<CardIDs>();
+            return hand;
+        }
+    }
+}
